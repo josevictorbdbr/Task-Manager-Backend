07dfd58d6bdf94c4267a59816bdf01b348aa6bcc
josevictorbdbr <josevictorbdbr@gmail.com>
2026-02-06 18:38:50 -0300

    added task model

---
+++ src/main/java/com/josevictor/taskmanager/model/Task.java
@@ -0,0 +1,29 @@
+package com.josevictor.taskmanager.model;
+
+public class Task {
+    private Long id;
+    private String title;
+    private boolean completed;
+
+    public Task(Long id, String title){
+        this.id = id;
+        this.title = title;
+        this.completed = false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void complete(){
+        this.completed = true;
+    }
+}
