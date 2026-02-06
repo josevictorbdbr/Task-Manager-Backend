b46fcf6ff36697b88206dda71ff6da7bbac2dcbe
josevictorbdbr <josevictorbdbr@gmail.com>
2026-02-06 18:30:37 -0300

    added basic controller to check

---
+++ src/main/java/com/josevictor/taskmanager/controller/TaskController.java
@@ -0,0 +1,15 @@
+package com.josevictor.taskmanager.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @GetMapping
+    public String healthCheck(){
+        return "Task API running";
+    }
+
+}
